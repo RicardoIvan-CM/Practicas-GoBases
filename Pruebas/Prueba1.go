c9d8de372c047a96adcabae3fabfa817ce8d37b4
Ricardo Iván Cruz Macías <ricardoivan.cruz@mercadolibre.com.mx>
2023-03-29 10:18:38 -0600

    Create Prueba1.go

---
+++ Pruebas/Prueba1.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type Point struct {
+	x, y int
+}
+
+func (p *Point) move(xd, yd int) {
+	p.x += xd //Es igual a  (*p).x += xd por la desreferenciacion automatica en la operacion selector (x.f)
+	p.y += yd
+}
+
+func main() {
+	p := Point{0, 2}
+	fmt.Println(p)
+	p.move(1, 2)
+	fmt.Println(p)
+}
